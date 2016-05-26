1c8c1c82ce07fbe1cacab7f918011686a3417cb3
LI <1605651971@qq.com>
2016-05-26 15:29:10 +0800

    商城项目Android端（as版本）

---
+++ app/src/main/java/com/xh/sh/xhspas/ui/MainActivity.java
@@ -0,0 +1,15 @@
+package com.xh.sh.xhspas.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.xh.sh.xhspas.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
